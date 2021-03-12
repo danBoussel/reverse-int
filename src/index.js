704285c4809c8271805fd96ccbc0b6976e193375
Daniil Boussel <daniil7142@gmail.com>
2021-03-12 13:56:02 -0500

    feat: implement reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-module.exports = function reverse (n) {
-  
+// const helpFunc = (str) => {
+//     if (str.length > 1) return str[str.length - 1] + helpFunc(str.slice(0, -1))
+//     return str
+// }
+// module.exports = function reverse(n) {
+//     if (n < 0) n = n * -1
+//     return helpFunc(n.toString()) * 1
+// }
+
+module.exports = function reverse(n) {
+    if (n < 0) {
+        n = n * -1
+    }
+    return [n.toString()].join('').split('').reverse().join('') * 1
+
 }
